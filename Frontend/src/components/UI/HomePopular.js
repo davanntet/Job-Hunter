bdea0eba9f052242648f8b4b0540071cbc094abd
Davann CR <130335633+davanntet@users.noreply.github.com>
2023-04-28 00:00:41 +0700

    HomePage:6.0

--- Frontend/src/components/UI/HomePopular.js
+++ Frontend/src/components/UI/HomePopular.js
@@ -1,39 +1,37 @@
-import PopularCard from "../UI/HomePopularCard"; 
+import PopularCard from "../UI/HomePopularCard";
 import IconNextLeft from '../../Image/IconNextLeft.png'
 import IconNextRight from '../../Image/IconNextRight.png'
-import { useState } from "react";
+
+
 
 
 export default function HomePopular() {
-    const [nextpre, setNextPre] = useState(0);
 
     const code = [<PopularCard value={1} key={1} />, <PopularCard key={2} value={2} />, <PopularCard key={3} value={3} />, <PopularCard key={4} value={4} />, <PopularCard key={5} />, <PopularCard key={6} />, <PopularCard key={7} />, <PopularCard key={8} />, <PopularCard key={9} />, <PopularCard key={10} />]
-    const nextHandler = () => {
-        nextpre<code.length-1&&setNextPre(nextpre + 1)
-    }
-    const prevHandler = () => {
-        nextpre>0&&setNextPre(nextpre - 1)
-    }
-    let slide = []
-    for (let i = nextpre; i < code.length; i++) {
-        slide.push(code[i])
-    }
+
     return <>
         <div className="w-full flex items-center px-6">
-            <div className="ml-auto" onClick={prevHandler}>
+            <div className="ml-auto" onClick={() => {
+                document.getElementById('popular-category').scrollLeft -= 200
+            }} id="prev">
                 <img className="w-7 cursor-pointer" src={IconNextLeft} alt="" />
             </div>
             <div className="col-span-10 w-11/12 overflow-hidden right-28">
-                <div className="mx-7 w-auto gap-y-10 grid grid-flow-col overflow-scroll justify-start items-center right-52  gap-x-10 mt-10 transition-all" id='popular-category'>
+                <div className="mx-7 h-60 scroll-smooth w-auto gap-y-10 grid grid-flow-col overflow-scroll justify-start items-center right-52  gap-x-10 mt-10 transition-all" id='popular-category' onScroll={(e) => {
+                    console.log(e.target.scrollLeft)
+                }}>
                     {
-                        slide.map(source=>source)
+                        code.map(source => source)
                     }
                 </div>
             </div>
-            <div className="mr-auto" onClick={nextHandler}>
+            <div className="mr-auto" onClick={() => {
+                document.getElementById('popular-category').scrollLeft += 200
+            }} id="next">
                 <img className="w-7 cursor-pointer" src={IconNextRight} alt="" />
             </div>
         </div>
-    
+
     </>
 }
+
--- Frontend/src/components/UI/HomePopularCard.js
+++ Frontend/src/components/UI/HomePopularCard.js
@@ -1,7 +1,7 @@
 import IconJob from '../../Image/IconJob.png'
 export default function PopularCard({value}) {
     return <>
-        <div className='p-3 border-2 border-one w-40 h-40 flex flex-col items-center gap-y-5 justify-center rounded-md'>
+        <div className='p-3 relative border-2 border-one w-40 h-40 flex flex-col items-center gap-y-5 justify-center rounded-md'>
             <div>
                 <img className='w-20 h-20' src={IconJob} alt='' />
             </div>
